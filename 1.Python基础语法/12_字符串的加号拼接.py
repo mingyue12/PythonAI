cddcb4d09aa837557ca1c63ff4ac9de41bb59b13
mingyue12 <108075525+mingyue12@users.noreply.github.com>
2025-12-26 22:01:59 +0800

    str

--- 1.Python基础语法/12_字符串的加号拼接.py
+++ 1.Python基础语法/12_字符串的加号拼接.py
@@ -9,4 +9,4 @@ info = name + "今年" + str(age) + "岁，身高" + str(height) + "米"
 print(info, type(info))
 
 # *用于数字是乘法，用于字符串是字符串重复.
-print("-"*50)
+print("-" * 50)
---
+++ 1.Python基础语法/13_字符串拼接练习题.py
@@ -0,0 +1,7 @@
+
+money = 1000
+name = "王大锤"
+salary = 100
+
+# 字符串拼接,当其他类型数据进行字符串拼接先进行字符串强制转换，str()
+message = "我是" + name + "，我的存款有" + str(money) + "元，工资是" + str(salary) + "元"
