66af1dff90f6f4fd4c8c254fe7bed86eb2878809
mingyue12 <108075525+mingyue12@users.noreply.github.com>
2025-12-26 21:47:59 +0800

    str

--- 1.Python基础语法/12_字符串的加号拼接.py
+++ 1.Python基础语法/12_字符串的加号拼接.py
@@ -1,10 +1,12 @@
-import unittest
 
+# +用于数字是加法，用于字符串是字符串拼接
+print("Hello " + "World")
+name = "周杰伦"
+age = 11
+height = 1.75
+# 拼装信息到info变量中
+info = name + "今年" + str(age) + "岁，身高" + str(height) + "米"
+print(info, type(info))
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
-
-
-if __name__ == '__main__':
-    unittest.main()
+# *用于数字是乘法，用于字符串是字符串重复
+print("-"*50)
