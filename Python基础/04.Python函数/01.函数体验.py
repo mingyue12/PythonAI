d7cb1eb31a0665cd24ee6aead0d2ffa50625fe82
mingyue12 <108075525+mingyue12@users.noreply.github.com>
2026-01-26 01:04:16 +0800

    Create 01.函数体验.py
    yes

---
+++ Python基础/04.Python函数/01.函数体验.py
@@ -0,0 +1,22 @@
+def my_len(data):
+    """
+    计算输入数据的长度
+    参数:
+        data: 需要计算长度的可迭代对象
+    返回:
+        data的长度
+    """
+    my_length: int = 0  # 初始化长度计数器为0
+    for i in data:  # 遍历data中的每个元素
+        my_length += 1  # 每遍历一个元素，长度计数器加1
+    return my_length  # 返回计算得到的长度
+
+
+name = "itheima"
+length = 0
+for i in name:
+    length += 1
+print(f"{name}的长度是{length}")
+
+info = "我爱学习"
+print(f"{info}的长度是{my_len(info)}")
