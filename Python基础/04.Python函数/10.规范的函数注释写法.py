4f310c5455ef04d12d082aa716178f0392a152af
mingyue12 <108075525+mingyue12@users.noreply.github.com>
2026-01-31 10:27:26 +0800

    Create 10.规范的函数注释写法.py
    yes

---
+++ Python基础/04.Python函数/10.规范的函数注释写法.py
@@ -0,0 +1,15 @@
+
+
+def add(x, y):
+    """
+    :param(parameter(参数缩写))
+    这个函数的功能是完成两数相加
+    :param x:   相加的第一个数字
+    :param y:   相加的第二个数字
+    :return:    返回相加的结果
+    """
+    return x + y
+
+
+add(1, 2)
+print()
