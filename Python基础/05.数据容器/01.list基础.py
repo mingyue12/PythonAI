803ec4c6ff6808c028b8bc8a367c220a5290f0f8
mingyue12 <108075525+mingyue12@users.noreply.github.com>
2026-01-31 14:09:49 +0800

    Create 01.list基础.py
    yes

---
+++ Python基础/05.数据容器/01.list基础.py
@@ -0,0 +1,19 @@
+
+
+# list字面量
+
+
+lst = [1, 2, 3, 4, 5]
+print(lst)
+
+lst2 = ["hello", "world", 123, 45.6, True]
+print(lst2)
+
+list3 = [1, "hello", 3.14, False, [10, 20, 30]]
+print(list3)
+
+# 嵌套列表
+nested_list = [[1, 2, 3], ["a", "b", "c"], [True, False]]
+print(nested_list)
+
+print(type(lst))
