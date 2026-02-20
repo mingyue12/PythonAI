54fc937499f29ca3eba1a9b3f42828a2451ded3f
mingyue12 <108075525+mingyue12@users.noreply.github.com>
2026-02-20 11:12:02 +0800

    yes
    yes

--- Python进阶/day01/09.魔法方法之str.py
+++ Python进阶/day01/09.魔法方法之str.py
@@ -10,29 +10,25 @@
 """
 
 
-# 需求：定义汽车类，默认属性为：color = '黑色'，number = 3
-# 1.定义汽车类
 class Car:
-    # 1.1 在魔法方法__init__()中初始化属性
-    def __init__(self):
-        # 1.1.1 初始化属性
-        print("init初始化函数")
-        # 1.1.2 初始化属性
-        self.color = '黑色'
-        self.number = 4
+    # 有参的__init__()函数，参数值由：外部对象自行赋值
+    def __init__(self, color, number):
+        """
+        该魔法方法用于属性初始化
+        :param color: 车的颜色
+        :param number: 车轮子的数量
+        """
+        self.color = color
+        self.number = number
 
     def show(self):
         print(f"我是{self}，我是{self.color}的，我有{self.number}个轮胎")
 
 
-# 创建汽车类对象
-print("-" * 34)
-c1 = Car()  # 会自动调用__init__()函数
-# 修改属性
-c1.color = '红色'
-c1.number = 6
-print(c1.color, c1.number)
-c1.show()
-print("-" * 34)
-c2 = Car()
-c2.show()
+    def __str__(self):
+        return f"我的颜色是{self.color}我有几个轮子{self.number}"
+
+
+# 创建对象
+c1 = Car(color='绿色', number=4)
+print(c1)
