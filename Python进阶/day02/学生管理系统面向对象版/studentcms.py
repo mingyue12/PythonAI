78ccc864c0112f934d38e7788e378f1d5c9a0ea9
mingyue12 <108075525+mingyue12@users.noreply.github.com>
2026-03-18 18:46:40 +0800

    yes
    yes

--- Python进阶/day02/学生管理系统面向对象版/studentcms.py
+++ Python进阶/day02/学生管理系统面向对象版/studentcms.py
@@ -6,9 +6,25 @@
 # 1.导入学生类
 from student import Student
 
+
 # 2.定义学生管理系统类
-class StudentCMS:
+class StudentCMS(object):
     # 2.通过魔法方法初始化学生管理系统对象
     def __init__(self):
-        # 创建空列表用于存储学生信息
-        self.stu_list = list()  # 学生对象
+        # 2.1创建一个空列表，用于存储学生对象
+        self.students = []
+
+    def show_menu(self):
+        print("*" * 23)
+        print("学生管理系统V2.0")
+        print("\t1.添加学生")
+        print("\t2.删除学生")
+        print("\t3.修改学生")
+        print("\t4.查询学生")
+        print("\t5.显示所有学生")
+        print("\t0.退出系统")
+        print("*" * 23)
+
+
+if __name__ == '__main__':
+    StudentCMS().show_menu()
