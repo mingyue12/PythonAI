0248c927d598d2d15784f70fcb77007a3c51a26a
mingyue12 <108075525+mingyue12@users.noreply.github.com>
2026-04-06 13:15:18 +0800

    Create 06.装饰器装饰_有参无返回的原函数.py
    yes

---
+++ Python进阶/day03/06.装饰器装饰_有参无返回的原函数.py
@@ -0,0 +1,23 @@
+"""
+案例：装饰器装饰_无参无返回的原函数
+细节：
+    装饰器的内部函数格式要和被装饰的原函数保持一致，
+    即：原函数无参无返回，装饰器的内部函数也必须是无参无返回
+"""
+
+# 需求：定义有参无返回的get_sum()求和函数，在不改变其代码的基础上，添加友好提示，
+# 定义装饰器
+def my_decorator(func_name):
+    # 定义内部函数
+    def func_inner(a, b):
+        # 添加提示信息（额外功能）
+        print("正在计算")
+        # 调用原函数
+        get_sum(a, b)
+    return func_inner
+
+# 定义原函数,有参无返回值
+@my_decorator
+def get_sum(a, b):
+    sum = a + b
+    print("和为：", sum)
