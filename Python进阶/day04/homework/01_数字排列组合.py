cf9411b6a4f75c0fda5c20bea9132fc91e35b351
mingyue12 <2542407204@qq.com>
2026-04-11 14:46:10 +0800

    Update 01_数字排列组合.py
    yes

--- Python进阶/day04/homework/01_数字排列组合.py
+++ Python进阶/day04/homework/01_数字排列组合.py
@@ -8,10 +8,25 @@
     3，数4不能开头，
     4，5行以内搞定(包括5行)
 """
-from itertools import count
+
+import copy
 
 count = 0
 for s in [str(i) for i in range(1234, 4322)]:
     if '1' in s and '2' in s and '3' in s and '4' in s and '13' not in s and '31' not in s and s[0] != '4':
         count += 1
         print(s, end='\n' if count % 5 == 0 else '\t')
+
+
+
+# 需求:已知列表：my_list = ['aa', 'bb', 'cc', 'bb', 'bb', 'bb', 'dd']
+my_list = ['aa', 'bb', 'cc', 'bb', 'bb', 'bb', 'dd']
+new_list = [s for s in my_list if s != 'bb']
+print(new_list)
+
+# 2
+my_list = ['aa', 'bb', 'cc', 'bb', 'bb', 'bb', 'dd']
+for s in my_list[:]:    # 切片，避免修改原列表
+    if s == 'bb':
+        my_list.remove(s)
+print(my_list)
