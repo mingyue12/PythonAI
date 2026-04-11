83ed22994ac9d715105487ba643ad7b7385d356a
mingyue12 <2542407204@qq.com>
2026-04-11 21:54:23 +0800

    Create 01.多线程特点_随机性.py
    yes

---
+++ Python进阶/day05/01.多线程特点_随机性.py
@@ -0,0 +1,34 @@
+"""
+案例：演示多线程特点
+随机性：线程的执行顺序是随机的，不能保证线程的执行顺序是固定的。
+
+多线程特点：
+    1.线程执行具有随机性，原因是因为CPU在做着高效的切换
+    2.默认情况下，主线程会等待子线程结束再结束
+    3.（同一个进程的线程之间，是共享数据的）
+    4.多线程操作共享数据，可能会出现安全问题，可以用互斥锁解决问题
+
+CPU调度资源的策略：
+    1.
+    2.
+"""
+# 需求：创建多个线程，多次运行，观察结果
+
+# 导包
+import threading
+import time
+
+# 1.定义多线程的目标函数
+def print_info():
+    # 1.1 休眠
+    time.sleep(0.2)
+    # 1.2 获取当前线程对象
+    current_thread = threading.current_thread()
+    # 1.3 打印当前线程
+    print(current_thread)
+
+# 测试
+if __name__ == '__main__':
+    for i in range(10):
+        t = threading.Thread(target=print_info)
+        t.start()
